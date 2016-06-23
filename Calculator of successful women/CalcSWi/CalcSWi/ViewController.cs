e068af818892e9318d06f5b0b39cc461cea93cb4
vladasp <vladalogin@ukr.net>
2016-06-23 13:46:05 +0300

    Added business logic

--- Calculator of successful women/CalcSWi/CalcSWi/ViewController.cs
+++ Calculator of successful women/CalcSWi/CalcSWi/ViewController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoreGraphics;
+using System;
 
 using UIKit;
 
@@ -6,6 +7,8 @@ namespace CalcSWi
 {
     public partial class ViewController : UIViewController
     {
+        int kids = 0;
+        int age = 0;
         public ViewController(IntPtr handle) : base(handle)
         {
         }
@@ -13,7 +16,9 @@ namespace CalcSWi
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
+            Clear(new object(), new EventArgs());
+            RunButton.TouchUpInside += Calculation;
+            ClearButton.TouchUpInside += Clear;
         }
 
         public override void DidReceiveMemoryWarning()
@@ -21,5 +26,151 @@ namespace CalcSWi
             base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
         }
+
+        #region Event Hendlers
+        void Clear(object sender, EventArgs e)
+        {
+            KidsText.BackgroundColor = UIColor.White;
+            AgeText.BackgroundColor = UIColor.White;
+            NameText.BackgroundColor = UIColor.White;
+
+            KidsText.Text = string.Empty;
+            AgeText.Text = string.Empty;
+            NameText.Text = string.Empty;
+
+            DogSwitch.On = false;
+            CatSwitch.On = false;
+            GirlSwitch.On = false;
+            BoySwitch.On = false;
+
+            ResultLabel.Text = string.Empty;
+        }
+
+        private void Calculation(object sender, EventArgs e)
+        {
+            KidsText.BackgroundColor = UIColor.White;
+            AgeText.BackgroundColor = UIColor.White;
+            NameText.BackgroundColor = UIColor.White;
+
+            int input = GetInputValue();
+
+            GetResult(input);
+        }
+        #endregion
+
+        #region Data checking
+        private void GetResult(int index)
+        {
+            switch (index)
+            {
+                case 1:
+                    ResultLabel.Text = NameText.Text + "! " + Results.Ansver1;
+                    break;
+                case 2:
+                    ResultLabel.Text = NameText.Text + "! " + Results.Ansver2;
+                    break;
+                case 31:
+                    ResultLabel.Text = NameText.Text + "! " + Results.Ansver31;
+                    break;
+                case 32:
+                    ResultLabel.Text = NameText.Text + "! " + Results.Ansver32;
+                    break;
+                case 4:
+                    ResultLabel.Text = NameText.Text + "! " + Results.Ansver4;
+                    break;
+                default:
+                    ResultLabel.Text = NameText.Text + "! " + Results.Ansver5;
+                    break;
+            }
+        }
+
+        private bool CanCalcKids()
+        {
+            if (KidsText.Text == string.Empty)
+            {
+                kids = 0;
+                return true;
+            }
+            else if (int.TryParse(KidsText.Text, out kids))
+            {
+                return kids >= 0;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool NormaAge()
+        {
+            return int.TryParse(AgeText.Text, out age) && age > 0 && age < 100;
+        }
+
+        private bool ClassicalOrientation()
+        {
+            return BoySwitch.On && GirlSwitch.On || GirlSwitch.On;
+        }
+
+        private bool CatLady()
+        {
+            return CatSwitch.On && DogSwitch.On;
+        }
+
+        private bool IsAdult()
+        {
+            return NormaAge() && age >= 18;
+        }
+
+        private bool IsAnotherPlanet()
+        {
+            return NameText.Text == string.Empty || DogSwitch.On && CatSwitch.On && BoySwitch.On && GirlSwitch.On;
+        }
+        #endregion
+
+        private int GetInputValue()
+        {
+            int input = 0;
+
+            if (!NormaAge() || !CanCalcKids())
+            {
+                AgeText.BackgroundColor = NormaAge() ? UIColor.White : UIColor.Orange;
+                KidsText.BackgroundColor = CanCalcKids() ? UIColor.White : UIColor.Orange;
+                NameText.BackgroundColor = (NameText.Text != string.Empty) ? UIColor.White : UIColor.Orange;
+                input = 5;
+            }
+            else if (IsAnotherPlanet())
+            {
+                NameText.BackgroundColor = (NameText.Text != string.Empty) ? UIColor.White : UIColor.Orange;
+                input = 4;
+            }
+            else if (!IsAdult())
+            {
+                input = 1;
+            }
+            else if (kids == 0)
+            {
+                if (CatLady() || !ClassicalOrientation() || Segments.SelectedSegment != 1)
+                {
+                    input = 2;
+                }
+                else if (Segments.SelectedSegment == 1)
+                {
+                    input = 32;
+                }
+            }
+            else if (kids > 0)
+            {
+                if (Segments.SelectedSegment == 2)
+                {
+                    input = 2;
+                }
+                else
+                {
+                    input = 31;
+                }
+            }
+            return input;
+        }
+
     }
 }
--- Calculator of successful women/CalcSWi/CalcSWi/ViewController.designer.cs
+++ Calculator of successful women/CalcSWi/CalcSWi/ViewController.designer.cs
@@ -1,17 +1,128 @@
-﻿//
-// This file has been generated automatically to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
 
 namespace CalcSWi
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField AgeText { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISwitch BoySwitch { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISwitch CatSwitch { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ClearButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISwitch DogSwitch { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISwitch GirlSwitch { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField KidsText { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField NameText { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel Resl { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel ResultLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton RunButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISegmentedControl Segments { get; set; }
+
+        void ReleaseDesignerOutlets ()
         {
+            if (AgeText != null) {
+                AgeText.Dispose ();
+                AgeText = null;
+            }
+
+            if (BoySwitch != null) {
+                BoySwitch.Dispose ();
+                BoySwitch = null;
+            }
+
+            if (CatSwitch != null) {
+                CatSwitch.Dispose ();
+                CatSwitch = null;
+            }
+
+            if (ClearButton != null) {
+                ClearButton.Dispose ();
+                ClearButton = null;
+            }
+
+            if (DogSwitch != null) {
+                DogSwitch.Dispose ();
+                DogSwitch = null;
+            }
+
+            if (GirlSwitch != null) {
+                GirlSwitch.Dispose ();
+                GirlSwitch = null;
+            }
+
+            if (KidsText != null) {
+                KidsText.Dispose ();
+                KidsText = null;
+            }
+
+            if (NameText != null) {
+                NameText.Dispose ();
+                NameText = null;
+            }
+
+            if (Resl != null) {
+                Resl.Dispose ();
+                Resl = null;
+            }
+
+            if (ResultLabel != null) {
+                ResultLabel.Dispose ();
+                ResultLabel = null;
+            }
+
+            if (RunButton != null) {
+                RunButton.Dispose ();
+                RunButton = null;
+            }
+
+            if (Segments != null) {
+                Segments.Dispose ();
+                Segments = null;
+            }
         }
     }
 }
